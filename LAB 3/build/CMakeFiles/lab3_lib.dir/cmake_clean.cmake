0311c0b11a07500d0f941306044ca389f1d46c9d
Sedimav <sdimav2006@gmail.com>
2025-11-06 21:20:56 +0300

    Add files via upload

---
+++ LAB 3/build/CMakeFiles/lab3_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_lib.dir/figures.c++.o"
+  "CMakeFiles/lab3_lib.dir/figures.c++.o.d"
+  "liblab3_lib.a"
+  "liblab3_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
