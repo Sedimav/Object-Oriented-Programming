11869e74f2f32f5574cefdcc34d960465c19972d
Sedimav <sdimav2006@gmail.com>
2025-11-06 20:35:37 +0300

    Add files via upload

---
+++ LAB 1/build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/test_turkish_number_converter.cpp.o"
+  "CMakeFiles/tests.dir/test_turkish_number_converter.cpp.o.d"
+  "CMakeFiles/tests.dir/turkish_number_converter.cpp.o"
+  "CMakeFiles/tests.dir/turkish_number_converter.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
