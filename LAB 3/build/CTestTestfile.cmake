42f0b38310a0eee40b4b116102cbecc838f81fd0
Sedimav <sdimav2006@gmail.com>
2025-11-06 21:17:47 +0300

    Add files via upload

---
+++ LAB 3/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sedimav/figures
+# Build directory: /Users/sedimav/figures/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(FigureTests "/Users/sedimav/figures/build/tests")
+set_tests_properties(FigureTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/sedimav/figures/CMakeLists.txt;26;add_test;/Users/sedimav/figures/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
