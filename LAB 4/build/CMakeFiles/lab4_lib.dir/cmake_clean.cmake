e91e2d26d50356a51146dc85042af837ad214d5b
Sedimav <sdimav2006@gmail.com>
2025-11-12 16:28:56 +0300

    Add files via upload

---
+++ LAB 4/build/CMakeFiles/lab4_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4_lib.dir/figures.c++.o"
+  "CMakeFiles/lab4_lib.dir/figures.c++.o.d"
+  "liblab4_lib.a"
+  "liblab4_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
