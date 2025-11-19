c447033580a93ad92a26dbcb43d8bdc9d1e19a99
Sedimav <sdimav2006@gmail.com>
2025-11-20 01:37:23 +0300

    Add files via upload

---
+++ LAB5/build/CMakeFiles/CoreLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoreLib.dir/func.c++.o"
+  "CMakeFiles/CoreLib.dir/func.c++.o.d"
+  "libCoreLib.a"
+  "libCoreLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoreLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
