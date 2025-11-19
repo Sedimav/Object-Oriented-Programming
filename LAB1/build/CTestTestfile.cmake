79bb256546e99fd98c0d043da34cdda12cf8dae7
Sedimav <sdimav2006@gmail.com>
2025-11-19 23:47:37 +0300

    Delete LAB1 directory

--- LAB1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sedimav/turkish_numbers
-# Build directory: /Users/sedimav/turkish_numbers/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TurkishNumberTests "/Users/sedimav/turkish_numbers/build/tests")
-set_tests_properties(TurkishNumberTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/sedimav/turkish_numbers/CMakeLists.txt;22;add_test;/Users/sedimav/turkish_numbers/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- LAB1/turkish_number_converter.h
+++
@@ -1,8 +0,0 @@
-#ifndef TURKISH_NUMBER_CONVERTER_H
-#define TURKISH_NUMBER_CONVERTER_H
-
-#include <string>
-
-std::string convert_to_turkish(int number);
-
-#endif
