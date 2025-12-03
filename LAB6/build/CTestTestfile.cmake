6dff1fc0a916f9f2468718b49a6506cc56509c2e
Sedimav <sdimav2006@gmail.com>
2025-12-04 01:31:19 +0300

    Add files via upload

---
+++ LAB6/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sedimav/Desktop/ООП/LAB6
+# Build directory: /Users/sedimav/Desktop/ООП/LAB6/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AllTests "/Users/sedimav/Desktop/ООП/LAB6/build/tests")
+set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/sedimav/Desktop/ООП/LAB6/CMakeLists.txt;16;add_test;/Users/sedimav/Desktop/ООП/LAB6/CMakeLists.txt;0;")
