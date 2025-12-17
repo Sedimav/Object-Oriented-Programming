c4d3f3b4289cb0476e8f6ee8faf5f4615e6a7e5c
Sedimav <sdimav2006@gmail.com>
2025-12-17 22:56:28 +0300

    Add files via upload

---
+++ LAB7/build/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/bull.cpp.o"
+  "CMakeFiles/tests.dir/bull.cpp.o.d"
+  "CMakeFiles/tests.dir/game.cpp.o"
+  "CMakeFiles/tests.dir/game.cpp.o.d"
+  "CMakeFiles/tests.dir/tests.cpp.o"
+  "CMakeFiles/tests.dir/tests.cpp.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAB7/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sedimav/Desktop/ООП/LAB7
+# Build directory: /Users/sedimav/Desktop/ООП/LAB7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Lab7Tests "/Users/sedimav/Desktop/ООП/LAB7/build/tests")
+set_tests_properties(Lab7Tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/sedimav/Desktop/ООП/LAB7/CMakeLists.txt;35;add_test;/Users/sedimav/Desktop/ООП/LAB7/CMakeLists.txt;0;")
